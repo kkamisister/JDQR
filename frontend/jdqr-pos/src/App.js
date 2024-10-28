873bc93e75de5d687ea3acefc84ab34c9180e19e
DESKTOP-MMCQPV1\SSAFY <kyp0416@gmail.com>
2024-10-28 11:27:06 +0900

    added: 로그인 페이지 작성
    로그인 페이지 작성 및 프로젝트 기본 설정
    
    Jira: S11P31A608-174

--- frontend/jdqr-pos/src/App.js
+++ frontend/jdqr-pos/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { SnackbarProvider } from 'notistack';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorPage from 'pages/error/ErrorPage';
+import LoginPage from 'pages/login/LoginPage';
+import MainLayout from 'layouts/MainLayout';
+const queryClient = new QueryClient();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ErrorFallback({ error }) {
+	return <ErrorPage message={error.message} />;
 }
+
+const App = () => {
+	return (
+		<ErrorBoundary FallbackComponent={ErrorFallback}>
+			<SnackbarProvider autoHideDuration={2000}>
+				<QueryClientProvider client={queryClient}>
+					<BrowserRouter>
+						<Routes>
+							{/* <Route path="/" element={<HomePage />} /> */}
+							<Route element={<MainLayout />}>
+								<Route path="/login" element={<LoginPage />} />
+							</Route>
+							<Route
+								path="*"
+								element={
+									<ErrorPage message={'페이지를 찾을 수 없습니다'} />
+								}
+							/>
+						</Routes>
+					</BrowserRouter>
+					<ReactQueryDevtools />
+				</QueryClientProvider>
+			</SnackbarProvider>
+		</ErrorBoundary>
+	);
+};
 
 export default App;

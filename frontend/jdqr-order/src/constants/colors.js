e353c10e85871a55b9a4fb6a4d92c7386c81680b
김하연 <hayeonful@gmail.com>
2024-10-28 17:21:00 +0900

    feat: colors.js 설정
    Jira: S11P31A608-155

---
+++ frontend/jdqr-order/src/constants/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  main: {
+    primary100: "#FFD8CF",
+    primary200: "#FFB19F",
+    primary300: "#FF9D87",
+    primary400: "#FF8A6F",
+    primary500: "#FF7758",
+    primary600: "#DA664B",
+    primary700: "#B6553E",
+    primary800: "#914432",
+    primary900: "#482219",
+  },
+};

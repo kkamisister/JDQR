89b44fd3e5ab269c4e09c71ddc0553c610561a9f
조용수 <soo_0201@naver.com>
2024-10-29 11:24:49 +0900

    feat: baseEntity 수정
    Jira: S11P31A608-196

--- backend/src/main/java/com/example/backend/common/entity/BaseEntity.java
+++ backend/src/main/java/com/example/backend/common/entity/BaseEntity.java
@@ -1,4 +1,25 @@
 package com.example.backend.common.entity;
 
-public class BaseEntity {
+import com.example.backend.common.enums.EntityStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "status")
+    private EntityStatus status;
+
+    @PrePersist
+    public void prePersist() {
+        this.status = EntityStatus.ACTIVE;
+    }
 }

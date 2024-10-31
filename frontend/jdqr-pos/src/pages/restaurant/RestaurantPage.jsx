b9c1841709840aa265d5183d554152a208430d07
DESKTOP-MMCQPV1\SSAFY <kyp0416@gmail.com>
2024-10-31 15:20:46 +0900

    feat: 기능별 페이지 템플릿 작성
    Jira: S11P31A608-216

---
+++ frontend/jdqr-pos/src/pages/restaurant/RestaurantPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RestaurantPage = () => {
+	return <div></div>;
+};
+
+export default RestaurantPage;
---
+++ frontend/jdqr-pos/src/pages/table/TablePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+const TablePage = () => {
+	return (
+		<Stack direction="column">
+			<Stack direction="row"></Stack>
+		</Stack>
+	);
+};
+
+export default TablePage;

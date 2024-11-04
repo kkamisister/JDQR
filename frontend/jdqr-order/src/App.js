6a5cb1fdeb02660c57cb10916b95f6c0f7aaac8a
김하연 <hayeonful@gmail.com>
2024-11-04 15:20:46 +0900

    chore: notistack 설치

--- frontend/jdqr-order/src/App.js
+++ frontend/jdqr-order/src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/place/HomePage";
 import DishPage from "./pages/dish/DishPage";
 import CartPage from "./pages/cart/CartPage";
+import { SnackbarProvider } from "notistack";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dish" element={<DishPage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-    </BrowserRouter>
+    <SnackbarProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dish" element={<DishPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+    </SnackbarProvider>
   );
 }
 

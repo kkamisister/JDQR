f38a0984dffc86e64e7e7fcdf46a1132bda4426d
조용수 <soo_0201@naver.com>
2024-11-04 14:38:16 +0900

    feat: 결제 관련 dto 추가
    Jira: S11P31A608-222

--- backend/src/main/java/com/example/backend/order/dto/CartResponse.java
+++ backend/src/main/java/com/example/backend/order/dto/CartResponse.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 public record CartResponse() {
 
@@ -16,5 +17,14 @@ public record CartResponse() {
 		public static CartInfo of(Map<String, Map<Integer,CartDto>> userCartMap, String tableName, int peopleCnt) {
 			return new CartInfo(userCartMap,tableName,peopleCnt);
 		}
+	}
+
+	@Schema(name = "토스 결제 요청 데이터", description = "서버에서 저장한 주문 번호와 수량을 담는 response dto")
+	@Builder
+	public record InitialPaymentResponseDto(
+		String tossOrderId,
+		Integer amount
+	) {
+
 	}
 }

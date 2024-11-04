49cc27931be86e6c45add1060e970dec27e7f764
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-11-04 16:50:52 +0900

    feat: PeopliFilter 인원수 선택, 함께앉기 선택 로직 추가
    인원수 선택시에만 함께앉기 체크 가능
    
    Jira: S11P31A608-265

--- frontend/jdqr-order/src/pages/place/PeopleFilter.jsx
+++ frontend/jdqr-order/src/pages/place/PeopleFilter.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react"
 import { colors } from "../../constants/colors"
-import { Button, Stack, Typography } from "@mui/material"
+import { Button, Checkbox, Stack, Typography } from "@mui/material"
 import PeopleFilterButton from "../../components/button/PeopleFilterButton"
 
 const PeopleFilter = () => {
   const [activeIndex, setActiveIndex] = useState(null)
+  const [isChecked, setIsChecked] = useState(false)
 
   const handleButtonClick = (index) => {
-    setActiveIndex(index)
+    setActiveIndex((prevIndex) => {
+      const newIndex = prevIndex === index ? null : index
+
+      if (newIndex === null) {
+        setIsChecked(false)
+      } else {
+        setIsChecked(true)
+      }
+      console.log(isChecked)
+      return newIndex
+    })
   }
+
+  const isCheckboxDisabled = activeIndex === null
+
   return (
     <Stack direction="column" spacing={1}>
       <Stack
@@ -23,19 +37,44 @@ const PeopleFilter = () => {
           <PeopleFilterButton
             key={index}
             name={name}
-            isActive={activeIndex === index} // 현재 버튼이 활성화 상태인지 확인
-            onClick={() => handleButtonClick(index)} // 버튼 클릭 시 핸들러 호출
+            isActive={activeIndex === index}
+            onClick={() => handleButtonClick(index)}
           />
         ))}
       </Stack>
       <Stack
         direction="row"
-        spacing={2}
+        // spacing={1}
         alignItems="center"
         paddingLeft="10px"
         whiteSpace="nowrap"
       >
-        <Typography fontSize={15}>함게 앉기</Typography>
+        <Typography
+          fontSize={15}
+          color={
+            isCheckboxDisabled ? colors.background.box : colors.main.primary400
+          }
+        >
+          함께 앉기
+        </Typography>
+        <Checkbox
+          checked={isChecked}
+          sx={{
+            color: colors.background.box,
+            "&.Mui-checked": {
+              color: colors.main.primary400,
+            },
+            "&.Mui-disabled": {
+              color: colors.background.box, // 비활성화 상태의 색상
+              opacity: 0.5, // 비활성화 상태의 불투명도
+            },
+            "& .MuiSvgIcon-root": {
+              fontSize: 25,
+            },
+          }}
+          disabled={isCheckboxDisabled}
+          onChange={(e) => setIsChecked(e.target.checked)}
+        />
       </Stack>
     </Stack>
   )

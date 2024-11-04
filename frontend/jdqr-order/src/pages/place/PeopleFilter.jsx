bd78d6eda5ebe8db7c355748e1ea3e949698bf20
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-11-04 15:15:46 +0900

    feat: PeopleFilterButton 컴포넌트 추가
    PeopleFilterButton 컴포넌트 추가, css 적용
    PeopleFilter 인원수 필터링  PeopleFilterButton 컴포넌트 이용하는 것으로 수정
    
    Jira: S11P31A608-265

--- frontend/jdqr-order/src/pages/place/PeopleFilter.jsx
+++ frontend/jdqr-order/src/pages/place/PeopleFilter.jsx
@@ -1,15 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 import { colors } from "../../constants/colors"
-import { Chip, Stack, Typography } from "@mui/material"
+import { Button, Stack, Typography } from "@mui/material"
+import PeopleFilterButton from "../../components/button/PeopleFilterButton"
 
 const PeopleFilter = () => {
+  const [activeIndex, setActiveIndex] = useState(null)
+
+  const handleButtonClick = (index) => {
+    setActiveIndex(index)
+  }
   return (
-    <Stack direction="row" spacing={1} alignItems="center">
-      <Typography fontSize={20}>인원 수</Typography>
-      <Chip label="1명"></Chip>
-      <Chip label="2명"></Chip>
-      <Chip label="4명"></Chip>
-      <Chip label="단체"></Chip>
+    <Stack direction="column" spacing={1}>
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        paddingLeft="10px"
+        whiteSpace="nowrap"
+      >
+        <Typography fontSize={15}>인원 수</Typography>
+        {["1인", "2인", "4인", "단체"].map((name, index) => (
+          <PeopleFilterButton
+            key={index}
+            name={name}
+            isActive={activeIndex === index} // 현재 버튼이 활성화 상태인지 확인
+            onClick={() => handleButtonClick(index)} // 버튼 클릭 시 핸들러 호출
+          />
+        ))}
+      </Stack>
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        paddingLeft="10px"
+        whiteSpace="nowrap"
+      >
+        <Typography fontSize={15}>함게 앉기</Typography>
+      </Stack>
     </Stack>
   )
 }

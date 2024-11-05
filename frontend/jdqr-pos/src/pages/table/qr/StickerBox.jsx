5149ac8d86ae8c7e8a4af3b84744fad8e8b346cb
DESKTOP-MMCQPV1\SSAFY <kyp0416@gmail.com>
2024-11-05 15:41:30 +0900

    feat: QR코드 정보 Dialog 작성
    Jira: S11P31A608-215

---
+++ frontend/jdqr-pos/src/pages/table/qr/StickerBox.jsx
@@ -0,0 +1,44 @@
+import React, { forwardRef } from 'react';
+import { Stack, Box } from '@mui/material';
+import RestaurantLogoSample from 'assets/images/RestaurantLogoSample.png';
+import ImageBox from 'components/common/ImageBox';
+import { colors } from 'constants/colors';
+import QRCodeBox from './QRCodeBox';
+const StickerBox = forwardRef(({ table }, ref) => {
+	return (
+		<Stack
+			direction="column"
+			spacing={2}
+			ref={ref}
+			sx={{
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: colors.main.primary300,
+				color: colors.text.white,
+				borderRadius: '15px',
+				padding: '15px',
+				width: '200px',
+				height: '300px',
+			}}>
+			<Stack
+				direction="row"
+				spacing={1}
+				sx={{ justifyContent: 'center', alignItems: 'center' }}>
+				<ImageBox
+					borderRadius="10px"
+					sx={{ height: '40px', width: 'auto' }}
+					src={RestaurantLogoSample}
+					alt={'가게 이미지'}
+				/>
+				<Box sx={{ fontWeight: '600', fontSize: '30px' }}>{table.name}</Box>
+			</Stack>
+
+			<QRCodeBox url={'https://naver.com'} />
+			<Box sx={{ fontWeight: '600', fontSize: '25px' }}>
+				QR코드로 주문하기
+			</Box>
+		</Stack>
+	);
+});
+
+export default StickerBox;

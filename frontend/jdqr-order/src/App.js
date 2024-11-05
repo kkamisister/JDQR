f71ebc2d923280f84ec7a426a7a8c323b51adf31
김하연 <hayeonful@gmail.com>
2024-11-05 19:48:01 +0900

    feat: 결제 페이지 생성
    Jira: S11P31A608-282

--- frontend/jdqr-order/src/App.js
+++ frontend/jdqr-order/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/place/HomePage";
 import DishPage from "./pages/dish/DishPage";
 import CartPage from "./pages/cart/CartPage";
+import PaymentPage from "./pages/payment/PaymentPage";
 import { SnackbarProvider } from "notistack";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/dish" element={<DishPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
         </Routes>
       </BrowserRouter>
     </SnackbarProvider>

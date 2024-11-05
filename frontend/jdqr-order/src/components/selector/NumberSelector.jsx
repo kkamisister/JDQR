99b8a4ad9823ed56ef6b378ab2454b1a01930737
김하연 <hayeonful@gmail.com>
2024-11-05 11:06:59 +0900

    design: number selector 스타일 수정
    Jira: S11P31A608-279

--- frontend/jdqr-order/src/components/selector/NumberSelector.jsx
+++ frontend/jdqr-order/src/components/selector/NumberSelector.jsx
@@ -1,24 +1,47 @@
-import { IconButton, TextField, Stack } from "@mui/material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { colors } from "../../constants/colors";
 
-export default function NumberSelector({ value, setValue, sx }) {
+export default function NumberSelector({ value = 1, setValue, sx }) {
   const handleIncrease = () => setValue(value + 1);
-  const handleDecrease = () => setValue(value > 0 ? value - 1 : 0);
+  const handleDecrease = () => setValue(value > 1 ? value - 1 : 1);
 
   return (
-    <Stack direction="row" alignItems="center" spacing={1} sx={{ ...sx }}>
-      <IconButton onClick={handleDecrease}>
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        ...sx,
+        bgcolor: sx?.bgcolor || colors.background.box,
+        width: sx?.width || "90px",
+        borderRadius: 2,
+        py: 0.5,
+      }}
+    >
+      <IconButton
+        onClick={handleDecrease}
+        sx={{ p: 0, color: colors.text.sub1 }}
+      >
         <RemoveIcon />
       </IconButton>
-      <TextField
-        type="number"
-        value={value}
-        onChange={(e) => setValue(Number(e.target.value))}
-        inputProps={{ min: 0 }}
-        sx={{ width: 60 }}
-      />
-      <IconButton onClick={handleIncrease}>
+
+      <Typography
+        sx={{
+          color: colors.text.main,
+          bgcolor: colors.background.white,
+          width: "40%",
+          borderRadius: 2,
+          textAlign: "center",
+        }}
+      >
+        {value}
+      </Typography>
+      <IconButton
+        onClick={handleIncrease}
+        sx={{ p: 0, color: colors.text.sub1 }}
+      >
         <AddIcon />
       </IconButton>
     </Stack>

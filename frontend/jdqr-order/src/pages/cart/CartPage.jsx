7bb4e7ff231a910d78447fd6a9970d8c2bb3ed45
김하연 <hayeonful@gmail.com>
2024-11-05 09:29:42 +0900

    feat: 장바구니 페이지 추가
    Jira: S11P31A608-277

--- frontend/jdqr-order/src/pages/cart/CartPage.jsx
+++ frontend/jdqr-order/src/pages/cart/CartPage.jsx
@@ -1,10 +1,13 @@
 import Header from "../../components/header/Header";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import { colors } from "../../constants/colors";
+import CartList from "./CartList";
 
 export default function CartPage() {
   return (
-    <Box>
+    <Box sx={{ bgcolor: colors.background.box, height: "100vh" }}>
       <Header title="장바구니" BackPage={true} />
+      <CartList />
     </Box>
   );
 }

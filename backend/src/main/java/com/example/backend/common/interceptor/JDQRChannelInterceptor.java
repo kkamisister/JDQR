e002a9148ccaa738c6914563002ae34a1d972b15
김경현 <sujipark2009@gmail.com>
2024-11-05 19:02:49 +0900

    feat: 테이블에 접속한 유저 확인로직 추가
    CONNECT시 redis에서 카운팅
    
    Jira: S11P31A608-246

--- backend/src/main/java/com/example/backend/common/interceptor/JDQRChannelInterceptor.java
+++ backend/src/main/java/com/example/backend/common/interceptor/JDQRChannelInterceptor.java
@@ -1,12 +1,16 @@
 // JDQRChannelInterceptor.java
 package com.example.backend.common.interceptor;
 
+import static com.example.backend.common.enums.OnlineUser.*;
+
+import com.example.backend.common.enums.OnlineUser;
 import com.example.backend.common.exception.ErrorCode;
 import com.example.backend.common.exception.JDQRException;
 import com.example.backend.common.util.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.*;
 import org.springframework.messaging.simp.stomp.*;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -20,6 +24,7 @@ public class JDQRChannelInterceptor implements ChannelInterceptor {
 
 	private final TokenProvider tokenProvider;
 	private static final String TOKEN_PREFIX = "Bearer ";
+	private final RedisTemplate<String,Object> redisTemplate;
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -35,6 +40,8 @@ public class JDQRChannelInterceptor implements ChannelInterceptor {
 
 				if(accessToken.equals("dummyTableToken")){
 					accessor.getSessionAttributes().put("tableId","6721aa9b0d22a923091eef73");
+					// 인원 수 증가
+					incrementOnlineUserCount("6721aa9b0d22a923091eef73");
 					return message;
 				}
 
@@ -49,6 +56,8 @@ public class JDQRChannelInterceptor implements ChannelInterceptor {
 					String tableId = tokenProvider.extractSubject(accessToken);
 					// 세션 속성에 사용자 ID 저장
 					accessor.getSessionAttributes().put("tableId", tableId);
+					// 인원 수 증가
+					incrementOnlineUserCount("6721aa9b0d22a923091eef73");
 				} else {
 					throw new JDQRException(ErrorCode.TOKEN_IS_NOT_VALID);
 				}
@@ -60,4 +69,29 @@ public class JDQRChannelInterceptor implements ChannelInterceptor {
 
 		return message;
 	}
+	@Override
+	public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+		// DISCONNECT 프레임에 대해서만 처리
+		if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
+			decrementOnlineUserCount("6721aa9b0d22a923091eef73");  // 인원수 감소
+		}
+	}
+
+	private void incrementOnlineUserCount(String tableId) {
+		log.warn("인원수 증가 !!!!");
+		redisTemplate.opsForHash().increment(ONLINE_USER.getExplain(), tableId, 1);
+	}
+
+	private void decrementOnlineUserCount(String tableId) {
+		log.warn("인원수 감소 !!!!");
+		Integer currentCount = (Integer) redisTemplate.opsForHash().get(ONLINE_USER.getExplain(), tableId);
+
+		// currentCount가 null이거나 1 이상일 때만 감소시킴
+		if (currentCount != null && currentCount > 0) {
+			redisTemplate.opsForHash().increment(ONLINE_USER.getExplain(), tableId, -1);
+		}
+	}
+
 }

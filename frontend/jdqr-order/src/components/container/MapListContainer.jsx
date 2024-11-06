659bec7976728c374e3d0159312e4138c18371a9
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-11-06 10:59:16 +0900

    rename: MapListBox MaplistContainer로 컴포넌트명 변경
    Jira:

--- frontend/jdqr-order/src/components/container/MapListContainer.jsx
+++ frontend/jdqr-order/src/components/container/MapListContainer.jsx
@@ -2,20 +2,20 @@ import React from "react"
 import { Stack } from "@mui/material"
 import { colors } from "../../constants/colors"
 
-const MapListBox = ({ children, sx }) => {
+const MapListContainer = ({ children, sx, height }) => {
   return (
     <Stack
       direction="column"
       spacing={2}
       sx={{
-        position: "sticky",
-        bottom: 0,
+        position: "relative", // 기존 "sticky"에서 변경
         width: "100%",
         backgroundColor: colors.background.white,
         borderTopLeftRadius: "35px",
         borderTopRightRadius: "35px",
         padding: "10px",
         overflowY: "auto",
+        height: height ? `${height}px` : "auto", // 높이 prop 적용
         ...sx,
       }}
     >
@@ -24,4 +24,4 @@ const MapListBox = ({ children, sx }) => {
   )
 }
 
-export default MapListBox
+export default MapListContainer

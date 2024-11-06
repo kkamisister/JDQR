ecbc23e33e28ae36f62443965361ed40acba5180
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-11-06 11:09:38 +0900

    init: RestaurantDeatilPage 초기 생성
    RestaurantDetailPage, RestaurantInfo 생성
    
    Jira: S11P31A608-268

--- frontend/jdqr-order/src/pages/place/detail/RestaurantDetailPage.jsx
+++ frontend/jdqr-order/src/pages/place/detail/RestaurantDetailPage.jsx
@@ -1,8 +1,30 @@
-import { Stack } from "@mui/material"
 import React from "react"
+import { Stack } from "@mui/material"
+import MapListContainer from "../../../components/container/MapListContainer"
+import RestaurantInfo from "./RestaurantInfo"
 
 const RestaurantDetailPage = () => {
-  return <Stack></Stack>
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        marginTop: "105px",
+        overflowY: "auto",
+      }}
+    >
+      <MapListContainer
+        sx={{
+          backgroundColor: "white",
+          height: "50vh",
+        }}
+      >
+        <RestaurantInfo />
+      </MapListContainer>
+    </div>
+  )
 }
 
 export default RestaurantDetailPage

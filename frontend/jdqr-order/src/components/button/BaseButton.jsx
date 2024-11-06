bae84e5a46a7afdd47d9422912344e2428743473
김하연 <hayeonful@gmail.com>
2024-11-06 10:47:04 +0900

    design: BaseButton position 변경
    sticky-> fixed
    가운데 정렬
    
    Jira: S11P31A608-279

--- frontend/jdqr-order/src/components/button/BaseButton.jsx
+++ frontend/jdqr-order/src/components/button/BaseButton.jsx
@@ -3,44 +3,41 @@ import { colors } from "../../constants/colors";
 
 export default function BaseButton({ count, onClick, children }) {
   return (
-    <Stack
+    <Button
+      onClick={onClick}
       sx={{
-        position: "sticky",
+        position: "fixed",
         bottom: 20,
-        alignItems: "center",
+        left: 0,
+        right: 0,
+        marginX: "auto",
+        bgcolor: colors.main.primary500,
+        borderRadius: "10px",
+        height: "45px",
+        width: "80%",
       }}
     >
-      <Button
-        onClick={onClick}
-        sx={{
-          bgcolor: colors.main.primary500,
-          borderRadius: "10px",
-          height: "45px",
-          width: "80%",
-        }}
-      >
-        <Stack direction="row" alignItems="center" mx={2}>
-          <Box
-            sx={{
-              bgcolor: colors.background.white,
-              color: colors.main.primary500,
-              fontSize: 18,
-              borderRadius: 1,
-              minWidth: 30,
-            }}
-          >
-            {count}
-          </Box>
-          <Typography
-            sx={{
-              color: colors.text.white,
-              px: 1,
-            }}
-          >
-            {children}
-          </Typography>
-        </Stack>
-      </Button>
-    </Stack>
+      <Stack direction="row" alignItems="center" mx={2}>
+        <Box
+          sx={{
+            bgcolor: colors.background.white,
+            color: colors.main.primary500,
+            fontSize: 18,
+            borderRadius: 1,
+            minWidth: 30,
+          }}
+        >
+          {count}
+        </Box>
+        <Typography
+          sx={{
+            color: colors.text.white,
+            px: 1,
+          }}
+        >
+          {children}
+        </Typography>
+      </Stack>
+    </Button>
   );
 }

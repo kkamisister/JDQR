300e8ae3c26bbe49a50d6f9e16b92aaa028b5980
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-11-06 13:23:42 +0900

    choer: RestaurantDetailPage 라우팅 추가
    Jira: S11P31A608-268

--- frontend/jdqr-order/src/App.js
+++ frontend/jdqr-order/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/place/main/HomePage"
+import RestaurantDetailPage from "./pages/place/detail/RestaurantDetailPage"
 import DishPage from "./pages/dish/DishPage"
 import CartPage from "./pages/cart/CartPage"
 import { SnackbarProvider } from "notistack"
@@ -10,6 +11,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route
+            path="/restaurant/detail/:restaurantId"
+            element={<RestaurantDetailPage />}
+          />
           <Route path="/dish" element={<DishPage />} />
           <Route path="/cart" element={<CartPage />} />
         </Routes>

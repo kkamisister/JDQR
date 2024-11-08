cfbfb959790127e3bb59d052f1463358f48a73b9
김하연 <hayeonful@gmail.com>
2024-11-08 18:02:47 +0900

    feat: query client provider 설정

--- frontend/jdqr-order/src/App.js
+++ frontend/jdqr-order/src/App.js
@@ -6,23 +6,28 @@ import PaymentPage from "./pages/payment/PaymentPage";
 import { SnackbarProvider } from "notistack";
 import HomePage from "./pages/place/main/HomePage";
 import DishDetailPage from "./pages/dish/detail/DishDetailPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <SnackbarProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/place" element={<HomePage />} />
-          <Route
-            path="/restaurant/detail/:restaurantId"
-            element={<RestaurantDetailPage />}
-          />
-          <Route path="/dish" element={<DishPage />} />
-          <Route path="/dish/:dishId" element={<DishDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/payment" element={<PaymentPage />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/place" element={<HomePage />} />
+            <Route
+              path="/restaurant/detail/:restaurantId"
+              element={<RestaurantDetailPage />}
+            />
+            <Route path="/dish" element={<DishPage />} />
+            <Route path="/dish/:dishId" element={<DishDetailPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/payment" element={<PaymentPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </SnackbarProvider>
   );
 }

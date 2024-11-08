77e15860d20e351875147e9965586632e033bf19
김하연 <hayeonful@gmail.com>
2024-11-08 18:02:47 +0900

    feat: query client provider 설정

--- frontend/jdqr-order/src/App.js
+++ frontend/jdqr-order/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import RestaurantDetailPage from "./pages/place/detail/RestaurantDetailPage"
 import DishPage from "./pages/dish/DishPage"
@@ -5,10 +6,24 @@ import CartPage from "./pages/cart/CartPage"
 import PaymentPage from "./pages/payment/PaymentPage"
 import { SnackbarProvider } from "notistack"
 import HomePage from "./pages/place/main/HomePage"
+=======
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RestaurantDetailPage from "./pages/place/detail/RestaurantDetailPage";
+import DishPage from "./pages/dish/DishPage";
+import CartPage from "./pages/cart/CartPage";
+import PaymentPage from "./pages/payment/PaymentPage";
+import { SnackbarProvider } from "notistack";
+import HomePage from "./pages/place/main/HomePage";
+import DishDetailPage from "./pages/dish/detail/DishDetailPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+>>>>>>> cfbfb95 (feat: query client provider 설정)
 
 function App() {
   return (
     <SnackbarProvider>
+<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path="/place" element={<HomePage />} />
@@ -21,6 +36,23 @@ function App() {
           <Route path="/payment" element={<PaymentPage />} />
         </Routes>
       </BrowserRouter>
+=======
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/place" element={<HomePage />} />
+            <Route
+              path="/restaurant/detail/:restaurantId"
+              element={<RestaurantDetailPage />}
+            />
+            <Route path="/dish" element={<DishPage />} />
+            <Route path="/dish/:dishId" element={<DishDetailPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/payment" element={<PaymentPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+>>>>>>> cfbfb95 (feat: query client provider 설정)
     </SnackbarProvider>
   )
 }

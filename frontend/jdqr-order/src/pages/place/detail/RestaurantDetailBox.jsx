e1a67701c2ee319f88a3c61ca1468dfd7cf2cf4c
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-11-10 16:27:49 +0900

    refactor: RestaurantDetailBox에서 RestaurantDetailDishItemCard map으로 순회 출력으로 수정
    Jira: S11P31A608-274

--- frontend/jdqr-order/src/pages/place/detail/RestaurantDetailBox.jsx
+++ frontend/jdqr-order/src/pages/place/detail/RestaurantDetailBox.jsx
@@ -2,12 +2,19 @@ import React from "react"
 import RestaurantInfo from "./RestaurantInfo"
 import MapListContainer from "../../../components/container/MapListContainer"
 import DishTab from "../../../components/tab/DishTab"
+import RestaurantDetailDishItemCard from "../../../components/card/RestaurantDetailDishItemCard"
 
 const RestaurantDetailBox = ({ categories, dishes }) => {
   return (
     <MapListContainer>
       <RestaurantInfo />
       <DishTab dishCategories={categories} />
+
+      {dishes.map((dishCategory) =>
+        dishCategory.items.map((dish) => (
+          <RestaurantDetailDishItemCard key={dish.dishId} dish={dish} />
+        ))
+      )}
     </MapListContainer>
   )
 }

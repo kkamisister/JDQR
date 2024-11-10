84c16e8602f8aee5bb4c85597ac6812d678e76ce
김경현 <sujipark2009@gmail.com>
2024-11-10 15:16:40 +0900

    fix: https설정
    -
    
    Jira: S11P31A608-252

--- backend/src/main/java/com/example/backend/common/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/backend/common/config/WebSocketConfig.java
@@ -35,7 +35,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 		//클라이언트에서 연결할 엔드포인트 설정
 		registry.addEndpoint("/ws")
-			.setAllowedOrigins("http://localhost:3000");
+			.setAllowedOrigins("https://localhost:3000");
 			// .withSockJS();
 	}
 

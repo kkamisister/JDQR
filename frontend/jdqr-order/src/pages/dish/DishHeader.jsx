692e9988c70be7db45e588d5c3607eb5afa0c616
김하연 <hayeonful@gmail.com>
2024-11-13 10:57:19 +0900

    feat: DishHeader 정보 동적 갱신
    장바구니 및 주문 인원 불러오기 위한 websocket 연결(아직 api 미완으로 데이터 업데이트 안 됨)
    주문 내역 api 연결
    
    Jira: S11P31A608-330, S11P31A608-317

--- frontend/jdqr-order/src/pages/dish/DishHeader.jsx
+++ frontend/jdqr-order/src/pages/dish/DishHeader.jsx
@@ -3,12 +3,55 @@ import { colors } from "../../constants/colors";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useNavigate } from "react-router-dom";
+import useWebSocketStore from "../../stores/SocketStore";
+import { setUserCookie, initializeToken } from "../../utils/apis/axiosInstance";
+import { useEffect, useState } from "react";
+import { fetchOrderList } from "../../utils/apis/order";
+import { useQuery } from "@tanstack/react-query";
 
 export default function DishHeader() {
   const navigate = useNavigate();
+  const { client, connect } = useWebSocketStore();
+  const [orderCnt, setOrderCnt] = useState(20);
+  let tableId = sessionStorage.getItem("tableId");
+
+  useEffect(() => {
+    if (!sessionStorage.getItem("userId")) {
+      setUserCookie();
+    }
+    if (!client) {
+      connect();
+    }
+  }, [client, connect]);
+
+  useEffect(() => {
+    if (!tableId || tableId === "undefined") {
+      initializeToken();
+      tableId = sessionStorage.getItem("tableId");
+    }
+    if (client && client.connected) {
+      console.log("일단 웹소켓 연결은 됨;;");
+      const subscription = client.subscribe("sub/cart" + tableId, (message) => {
+        console.log("이것이 멧쉐지", message.body);
+      });
+    }
+  });
+
+  useEffect(() => {
+    if (data) {
+      setOrderCnt(data.orders.length);
+    }
+  }, [data]);
+
   const goToCart = () => {
     navigate("/cart");
   };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["orderList"],
+    queryFn: fetchOrderList,
+  });
+
   return (
     <Stack
       spacing={2}
@@ -29,8 +72,9 @@ export default function DishHeader() {
             py: 1.5,
           }}
         >
-          {`주문 내역 ${0}건`}
+          {`주문 내역 ${orderCnt}건`}
         </Button>
+
         <Button
           startIcon={<ShoppingCartIcon />}
           onClick={goToCart}

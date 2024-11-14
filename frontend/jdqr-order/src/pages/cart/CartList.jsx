43ce27b7bd863720445de50b2771fe8baa99997d
김하연 <hayeonful@gmail.com>
2024-11-14 09:32:13 +0900

    refactor: Cart List Item 생성
    Cart List 내에 내가 담은 메뉴와 일행이 담은 메뉴 구분해서 담는 방식으로 변경하면서 메뉴 구분용 CartListItem 컴포넌트 생성
    
    Jira: S11P31A608-334, S11P31A608-346

--- frontend/jdqr-order/src/pages/cart/CartList.jsx
+++ frontend/jdqr-order/src/pages/cart/CartList.jsx
@@ -7,38 +7,36 @@ import { useNavigate } from "react-router-dom";
 import NumberSelector from "../../components/selector/NumberSelector";
 import { useSnackbar } from "notistack";
 import useWebSocketStore from "../../stores/SocketStore";
+import CartListItem from "./CartListItem";
+import BaseButton from "../../components/button/BaseButton";
+import Footer from "../../components/footer/Footer";
 
 export default function CartList() {
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
   const [dishes, setDishes] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantity, setTotalQuantity] = useState(0);
   const { client, connect } = useWebSocketStore();
   const tableId = sessionStorage.getItem("tableId");
+  const userId = sessionStorage.getItem("userId");
 
   useEffect(() => {
     if (!client) {
       connect();
     }
   }, [client, connect]);
-  useEffect(() => {
-    console.log("현재 상품", dishes);
-  }, [dishes]);
+
   useEffect(() => {
     if (client && client.connected) {
-      console.log(
-        "아~~웹소켓 연결은 제대로 됐다니까?/sub/cart/updates 한번..가볼게"
-      );
       const subscription = client.subscribe(
         "/sub/cart/" + tableId,
         (message) => {
-          console.log("받은메세지:", message.body);
           const data = JSON.parse(message.body);
-
+          console.log("내가 받은데이따", data.cartList);
           setDishes(data.cartList);
-
-          enqueueSnackbar(`${"아이템"}이 장바구니에 추가되었습니다.`, {
-            variant: "success",
-          });
+          setTotalPrice(data.totalPrice);
+          setTotalQuantity(data.totalQuantity);
         }
       );
 
@@ -46,20 +44,17 @@ export default function CartList() {
         subscription.unsubscribe();
       };
     }
-  }, [client, enqueueSnackbar]);
+  }, [client]);
 
-  const onClose = (dishId) => {
-    setDishes((prevDishes) => {
-      const updatedDishes = prevDishes.filter((_, index) => index !== dishId);
-      return updatedDishes;
-    });
+  const myDishes = dishes.filter((dish) => dish.userId === userId);
+  const othersDishes = dishes.filter((dish) => dish.userId !== userId);
 
-    enqueueSnackbar(`메뉴가 장바구니에서 삭제되었습니다.`, {
-      variant: "error",
-    });
-  };
   const goToDish = () => {
     navigate("/dish");
+  };
+
+  const goToPayment = () => {
+    navigate("/payment");
   };
 
   return (
@@ -70,53 +65,8 @@ export default function CartList() {
         mt: 2,
       }}
     >
-      <Stack>
-        <Typography
-          sx={{
-            fontWeight: 600,
-            fontSize: "16px",
-            p: "10px",
-          }}
-        >
-          담은 메뉴
-        </Typography>
-        {dishes.length > 0 &&
-          dishes.map((dish, dishId) => (
-            <Box key={dishId}>
-              <DishItemCard
-                dish={dish}
-                onClose={() => onClose(dishId)}
-                hasImage={false}
-                hasOption={true}
-              >
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  sx={{ alignSelf: "end", px: 2, pb: 1 }}
-                >
-                  <Button
-                    variant="disabled"
-                    sx={{
-                      bgcolor: colors.background.box,
-                      borderRadius: 2,
-                      fontSize: "12px",
-
-                      height: "25px",
-                      p: 0,
-                    }}
-                  >
-                    옵션 변경
-                  </Button>
-                  <NumberSelector
-                    value={dish.quantity}
-                    sx={{ width: "70px" }}
-                  />
-                </Stack>
-              </DishItemCard>
-              <Divider variant="middle" />
-            </Box>
-          ))}
-      </Stack>
+      <CartListItem title="내가 담은 메뉴" dishes={myDishes} />
+      <CartListItem title="일행이 담은 메뉴" dishes={othersDishes} />
       <Button
         endIcon={<AddCircleIcon />}
         onClick={goToDish}
@@ -129,6 +79,15 @@ export default function CartList() {
       >
         메뉴 더 담기
       </Button>
+      <Footer />
+
+      <Box>
+        {totalQuantity > 0 && totalPrice && (
+          <BaseButton count={totalQuantity} onClick={goToPayment}>
+            {`${totalPrice.toLocaleString()}원 결제하기`}
+          </BaseButton>
+        )}
+      </Box>
     </Stack>
   );
 }

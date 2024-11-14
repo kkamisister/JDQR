8594445973b2d14bb9c419a85b03f4684614ccc4
김하연 <hayeonful@gmail.com>
2024-11-14 17:10:00 +0900

    feat: 주문 내역 페이지 생성
    - 컴포넌트 단위 더 세부적으로 나누기
    - api 연결

--- frontend/jdqr-order/src/pages/order/OrderList.jsx
+++ frontend/jdqr-order/src/pages/order/OrderList.jsx
@@ -1,19 +1,28 @@
-import { Stack, Typography, Divider } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import OrderListItem from "./OrderListItem";
 import { fetchOrderList } from "../../utils/apis/order";
 import LoadingSpinner from "../../components/Spinner/LoadingSpinner";
 import { useQuery } from "@tanstack/react-query";
+import { colors } from "../../constants/colors";
+import BaseButton from "../../components/button/BaseButton";
 
 export default function OrderList() {
   const { data, isLoading, isError } = useQuery({
     queryKey: ["orderList"],
     queryFn: fetchOrderList,
   });
-
+  const goToPayment = () => {};
   return (
-    <Stack>
+    <Stack spacing={3} bgcolor={colors.background.box}>
       {isLoading && <LoadingSpinner message={"주문 내역 가져오는 중입니다."} />}
-      {!isLoading && !isError && <OrderListItem />}
+      {!isLoading && !isError && (
+        <Box>
+          {data.orders?.map((order, index) => (
+            <OrderListItem key={order.orderId} order={order} index={index} />
+          ))}
+          <BaseButton onClick={goToPayment}>결제하기</BaseButton>
+        </Box>
+      )}
     </Stack>
   );
 }

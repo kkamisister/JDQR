fcd76231f4a8fe72bf476c1f6c0b4a12de0a19f3
김하연 <hayeonful@gmail.com>
2024-11-16 15:07:13 +0900

    feat: 주문 상태 조회 및 변경 api 함수 추가

--- frontend/jdqr-order/src/utils/apis/order.js
+++ frontend/jdqr-order/src/utils/apis/order.js
@@ -27,3 +27,31 @@ export const placeOrder = async () => {
     throw new Error("이거 주문이 안되는데용???ㅠ,ㅠ");
   }
 };
+
+/**
+ * 주문상태 변경
+ * @returns {Object}
+ */
+
+export const ChangeOrderStatus = async () => {
+  try {
+    const response = await axiosInstance.post("order/status");
+    return response;
+  } catch (error) {
+    throw new Error("야;; 주문 상태 변경이 안된다..");
+  }
+};
+
+/**
+ * 주문 상태 조회
+ * @returns {Object}
+ */
+
+export const checkOrderStatus = async () => {
+  try {
+    const response = await axiosInstance.get("order/status");
+    return response;
+  } catch (error) {
+    throw new Error("흠냐 주문 상태 조회가 안되는데;;");
+  }
+};

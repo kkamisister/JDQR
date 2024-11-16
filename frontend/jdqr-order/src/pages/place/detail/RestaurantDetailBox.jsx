4183c7ed33043647835eec8e810f28ff2a4cab27
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-11-16 15:20:19 +0900

    design: RestaurantDetailBox css 수정
    Jira: S11P31A608-342

--- frontend/jdqr-order/src/pages/place/detail/RestaurantDetailBox.jsx
+++ frontend/jdqr-order/src/pages/place/detail/RestaurantDetailBox.jsx
@@ -1,20 +1,45 @@
 import React from "react"
+import { scroller } from "react-scroll"
 import RestaurantInfo from "./RestaurantInfo"
 import DishTab from "../../../components/tab/DishTab"
 import RestaurantDetailDishItemCard from "../../../components/card/RestaurantDetailDishItemCard"
-import { Stack } from "@mui/material"
+import { Box, Stack } from "@mui/material"
+import { colors } from "../../../constants/colors"
 
 const RestaurantDetailBox = ({ categories, dishes, restaurant }) => {
+  const handleCategoryClick = (category) => {
+    scroller.scrollTo(category, {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart",
+      containerId: "scrollable-dish-list",
+    })
+  }
   return (
     <Stack padding="10px" height="100%">
       <RestaurantInfo restaurant={restaurant} />
-      <DishTab dishCategories={categories} />
-
-      {dishes.map((dishCategory) =>
-        dishCategory.items.map((dish) => (
-          <RestaurantDetailDishItemCard key={dish.dishId} dish={dish} />
-        ))
-      )}
+      <DishTab dishCategories={categories} onTabClick={handleCategoryClick} />
+      <Box
+        id="scrollable-dish-list"
+        sx={{
+          flex: 1,
+          py: 1,
+          bgcolor: colors.background.box,
+          color: colors.text.main,
+          overflowY: "auto",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+          msOverflowStyle: "none",
+          scrollbarWidth: "none",
+        }}
+      >
+        {dishes.map((dishCategory) =>
+          dishCategory.items.map((dish) => (
+            <RestaurantDetailDishItemCard key={dish.dishId} dish={dish} />
+          ))
+        )}
+      </Box>
     </Stack>
   )
 }

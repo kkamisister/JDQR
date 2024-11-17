38c6055543c89058b7d4bc70b1b97b9ecc822d6b
김경현 <sujipark2009@gmail.com>
2024-11-17 22:24:15 +0900

    refactor: 기존 세션에서 재구독 방지
    -
    
    Jira: S11P31A608-359

--- backend/src/main/java/com/example/backend/common/event/JDQREventListener.java
+++ backend/src/main/java/com/example/backend/common/event/JDQREventListener.java
@@ -7,6 +7,7 @@ import static com.example.backend.order.dto.CartResponse.*;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.springframework.context.event.EventListener;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import com.example.backend.common.enums.Operator;
 import com.example.backend.common.exception.ErrorCode;
@@ -42,22 +44,35 @@ public class JDQREventListener {
 	public void sendData(CartEvent event) throws InterruptedException {
 		// 3. 전송할 데이터를 생성한다
 		String tableId = event.getTableId();
+		String sessionId = event.getSessionId();
 		Operator operator = event.getOperator();
+
 		// 3-1. 현재 테이블의 장바구니를 가지고온다.
 		// 맵은 (userId,해당 유저가 담은 항목들) 의 형식
 		Map<String, Map<Integer, CartDto>> allCartDatas = redisHashRepository.getAllCartDatas(tableId);
 
-		// 3-2. 현재 테이블과 연결된 사람수를 받아온다
-		log.warn("인원수 변화 !!!!");
+		// 처음 구독하는 세션인 경우만, 인원수 변경을 해야한다
 		Integer subscriberSize = null;
-		if(operator.equals(PLUS)){
+		if(!Boolean.TRUE.equals(redisTemplate.opsForSet().isMember("subscribed_sessions:" + sessionId, sessionId))){
+			redisTemplate.opsForSet().add("subscribed_sessions:" + sessionId, sessionId);
+			redisTemplate.expire("subscribed_sessions:" + sessionId, 10, TimeUnit.MINUTES);
+
+			log.warn("인원수 변화 !!!!");
 			subscriberSize = redisTemplate.opsForHash().increment(ONLINE_USER.getExplain(), tableId, 1).intValue();
 		}
-		else if(operator.equals(MINUS)){
-			subscriberSize = redisTemplate.opsForHash().increment(ONLINE_USER.getExplain(), tableId, -1).intValue();
-		}
+		else{ // 여기에 걸리는 경우는, 이미 구독한적이 있던 세션에서 이벤트를 날리는 경우이다. 그런 이벤트는 (SUBSCRIBE,DISCONNECT) 2가지가 있다
 
+			// SUBSCRIBE인 경우는 무시하고 DISCONNECT인 경우만 인원수를 줄여주면 된다
+			if(operator.equals(MINUS)){ // DISCONNECT 이벤트인 경우
+				log.warn("인원수 변화 !!!!");
+				subscriberSize = redisTemplate.opsForHash().increment(ONLINE_USER.getExplain(), tableId, -1).intValue();
+			}
+		}
+		if(ObjectUtils.isEmpty(subscriberSize)){
+			subscriberSize = redisHashRepository.getCurrentUserCnt(tableId);
+		}
 		if(subscriberSize < 0)subscriberSize = 0;
+
 		log.warn("현재 연결된 사람 수 :{}",subscriberSize);
 
 		// 3-3. 현재 테이블의 이름을 가져오기위해 테이블을 조회한다

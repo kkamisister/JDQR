3f9f31f90b9a87e6a0b727d7b18fddeafc1a93ff
김하연 <hayeonful@gmail.com>
2024-11-17 17:54:33 +0900

    design: 금액 분할 결제 ui 수정

--- frontend/jdqr-order/src/pages/payment/MoneyDivideInfo.jsx
+++ frontend/jdqr-order/src/pages/payment/MoneyDivideInfo.jsx
@@ -1,14 +1,26 @@
-import { Stack, Box, Input, Typography } from "@mui/material";
+import { Stack, Input, Typography } from "@mui/material";
 import { colors } from "../../constants/colors";
 import NumberSelector from "../../components/selector/NumberSelector";
 import { useState, useEffect } from "react";
 
-export default function MoneyDivideInfo({ initTotal, initPortion }) {
-  const [total, setTotal] = useState(initTotal);
-  const [portion, setPortion] = useState(initPortion);
+export default function MoneyDivideInfo({
+  initTotal,
+  initPortion,
+  totalPrice,
+}) {
+  const [total, setTotal] = useState(initTotal || 1);
+  const [portion, setPortion] = useState(initPortion || 1);
+  const [money, setMoney] = useState(0);
 
-  const money = 32423423.23123;
-  const bill = Math.ceil(money).toLocaleString();
+  useEffect(() => {
+    if (total > 0 && portion > 0) {
+      setMoney(totalPrice * (portion / total));
+      // console.log(totalPrice, "이게 토탈");
+      // console.log(money);
+    } else {
+      setMoney(0);
+    }
+  }, [total, portion, totalPrice]);
 
   return (
     <Stack
@@ -31,8 +43,8 @@ export default function MoneyDivideInfo({ initTotal, initPortion }) {
           disabled
           value={Math.ceil(money).toLocaleString()}
           endAdornment="원"
-          sx={{}}
         />
+        <Typography>{`(총액: ${totalPrice.toLocaleString()}원)`}</Typography>
       </Stack>
     </Stack>
   );
--- frontend/jdqr-order/src/pages/payment/MoneyDivideList.jsx
+++ frontend/jdqr-order/src/pages/payment/MoneyDivideList.jsx
@@ -1,24 +1,21 @@
-import { Stack } from "@mui/material";
+import { Stack, Box, Divider, Typography } from "@mui/material";
 import MoneyDivideInfo from "./MoneyDivideInfo";
+import DishItemCard from "../../components/card/DishItemCard";
+import { colors } from "../../constants/colors";
 
 const MoneyDivideList = ({ orders }) => {
-  console.log(orders);
   const dishes = orders.orders.flatMap((order) => order.dishes);
 
   const conjoinedDishes = dishes.reduce((acc, dish) => {
-    // 모든 옵션 정보를 key에 반영
     const key = `${dish.dishId}-${dish.options
       ?.map((option) => `${option.optionId}-${option.choiceId}`) // optionId-choiceId 형태로 변환
       .sort() // 옵션을 정렬
       .join("_")}`; // 정렬된 옵션을 병합
-    console.log(`${dish.dishName}의 key값은 ${key}`);
 
     if (acc[key]) {
-      // 동일한 key가 존재하면 totalPrice와 quantity를 합침
       acc[key].totalPrice += dish.price * dish.quantity;
       acc[key].quantity += dish.quantity;
     } else {
-      // 새로운 key 생성
       acc[key] = {
         dishId: dish.dishId,
         dishName: dish.dishName,
@@ -27,7 +24,7 @@ const MoneyDivideList = ({ orders }) => {
         quantity: dish.quantity,
         dishCategoryId: dish.dishCategoryId,
         dishCategoryName: dish.dishCategoryName,
-        options: [...dish.options], // 모든 옵션 정보 유지
+        options: [...dish.options],
       };
     }
 
@@ -35,11 +32,32 @@ const MoneyDivideList = ({ orders }) => {
   }, {});
 
   const conjoinedDishesArray = Object.values(conjoinedDishes);
-
-  console.log(conjoinedDishesArray);
+  // console.log(orders);
   return (
     <Stack>
-      <MoneyDivideInfo initTotal={orders.userCnt} initPortion={1} />
+      <MoneyDivideInfo
+        initTotal={orders.userCnt}
+        initPortion={1}
+        totalPrice={orders.price}
+      />
+      {conjoinedDishesArray.map((dish) => {
+        const optionDescription = dish.options
+          ?.map((option) => option.choiceName)
+          .join(", ");
+        return (
+          <Box>
+            <DishItemCard
+              dish={{ ...dish, description: optionDescription || "" }}
+              hasImage={false}
+            >
+              <Typography fontSize={12}>
+                총 수량 <span style={{ fontSize: 16 }}>{dish.quantity}</span>개
+              </Typography>
+            </DishItemCard>
+            <Divider variant="middle" />
+          </Box>
+        );
+      })}
     </Stack>
   );
 };

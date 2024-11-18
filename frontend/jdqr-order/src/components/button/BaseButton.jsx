2a6a697f866c8981915b370e68549111c5d8f837
김하연 <hayeonful@gmail.com>
2024-11-19 02:38:38 +0900

    feat: Basebutton에 disabled 추가

--- frontend/jdqr-order/src/components/button/BaseButton.jsx
+++ frontend/jdqr-order/src/components/button/BaseButton.jsx
@@ -1,37 +1,52 @@
 import { Box, Button, Typography, Stack } from "@mui/material";
 import { colors } from "../../constants/colors";
 
-export default function BaseButton({ count, onClick, children }) {
+export default function BaseButton({
+  count,
+  onClick,
+  disabled = false,
+  children,
+}) {
   return (
     <Button
       onClick={onClick}
+      disabled={disabled}
       sx={{
         position: "fixed",
         bottom: 20,
         left: 0,
         right: 0,
         marginX: "auto",
-        bgcolor: colors.main.primary500,
+        bgcolor: disabled ? colors.background.white : colors.main.primary500,
+        border: `1px solid ${
+          disabled ? colors.main.primary300 : "transparent"
+        }`,
         borderRadius: "10px",
         height: "45px",
         width: "80%",
+        color: disabled ? colors.main.primary500 : colors.text.white,
+        "& .MuiTypography-root": {
+          color: disabled ? colors.main.primary300 : colors.text.white,
+        },
+        "& .MuiBox-root": {
+          bgcolor: disabled ? colors.background.white : colors.main.primary500,
+          color: disabled ? colors.main.primary500 : colors.text.white,
+        },
       }}
     >
       <Stack direction="row" alignItems="center" mx={2}>
         <Box
           sx={{
-            bgcolor: colors.background.white,
-            color: colors.main.primary500,
             fontSize: 18,
             borderRadius: 1,
             minWidth: 30,
+            textAlign: "center",
           }}
         >
           {count}
         </Box>
         <Typography
           sx={{
-            color: colors.text.white,
             px: 1,
           }}
         >

4c7fdd95819c69e584bbfec913f4b8c9eee5a496
김경현 <sujipark2009@gmail.com>
2024-11-20 14:19:21 +0900

    refactor: 수정
    -
    
    Jira: S11P31A608-366

--- backend/src/test/java/com/example/backend/dish/repository/OptionRepositoryCustomImplTest.java
+++ backend/src/test/java/com/example/backend/dish/repository/OptionRepositoryCustomImplTest.java
@@ -1,9 +1,11 @@
 package com.example.backend.dish.repository;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,13 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.example.backend.TestDataGenerator;
 import com.example.backend.common.config.QuerydslConfig;
+import com.example.backend.dish.entity.Choice;
+import com.example.backend.dish.entity.Option;
 import com.example.backend.etc.entity.Restaurant;
 import com.example.backend.etc.repository.RestaurantRepository;
+import com.example.backend.owner.dto.OptionVo;
+import com.example.backend.owner.entity.Owner;
+import com.example.backend.owner.repository.OwnerRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,6 +39,8 @@ class OptionRepositoryCustomImplTest {
 	private OptionRepository optionRepository;
 	@Autowired
 	private ChoiceRepository choiceRepository;
+	@Autowired
+	private OwnerRepository ownerRepository;
 
 	private final TestDataGenerator generator = new TestDataGenerator();
 
@@ -39,7 +48,42 @@ class OptionRepositoryCustomImplTest {
 	@Test
 	void findAllOptionByRestaurant() {
 
-		// List<Restaurant> restaurants = generator.generateTestRestaurantList(false);
+		Owner owner = Owner.builder()
+			.email("sujipark2009@gmail.com")
+			.code("ABCDEFG")
+			.name("김영표")
+			.build();
+
+		Owner savedOwner = ownerRepository.save(owner);
+
+		List<Restaurant> restaurants = generator.generateTestRestaurantList(false);
+		for(Restaurant restaurant : restaurants) {
+			restaurant.setOwner(savedOwner);
+		}
+		List<Restaurant> restaurants1 = restaurantRepository.saveAll(restaurants);
+
+		List<Option> options = generator.generateTestOptionList(false);
+		for(Option option : options) {
+			option.setRestaurant(restaurants1.get(0));
+		}
+		optionRepository.saveAll(options);
+
+		List<Choice> choices = generator.generateTestChoiceList(false);
+		for(int i=0;i<choices.size();i++) {
+			Choice choice = choices.get(i);
+			choice.setOption(options.get(i));
+		}
+		choiceRepository.saveAll(choices);
+
+		//when
+		List<OptionVo> res = optionRepository.findAllOptionByRestaurant(restaurants1.get(0));
+
+		//then
+		assertThat(res)
+			.extracting(OptionVo::getOptionName)
+			.containsExactly(
+				"부위","맛","샷추가","휘핑크림","소스","양념"
+			);
 
 	}
 }
